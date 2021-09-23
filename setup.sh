5f4851e3164e2308aa5d689bb64a7c95370c7f6f
Ryan McAndrew <rrmcandrew@gmail.com>
2021-09-23 17:39:41 -0400

    add ignore for data in repo, edit dyno setup, remove load of large csv, add form, add expander

--- setup.sh
+++ setup.sh
@@ -1,16 +1,16 @@
 mkdir -p ~/.streamlit/
 
 echo "\
-[server]\n\
-headless = true\n\
-port = $PORT\n\
-enableCORS = false\n\
-# [theme]\n\
-# primaryColor = '#7792E3'\n\
-# backgroundColor = '#273346'\n\
-# secondaryBackgroundColor = '#73abd6'\n\
-# textColor = '#FFFFFF'\n\
-# font = 'sans serif'\n\
-\n\
+[server]
+headless = true
+port = $PORT
+enableCORS = false
+[theme]
+primaryColor = '#7792E3'
+backgroundColor = '#273346'
+secondaryBackgroundColor = '#73abd6'
+textColor = '#FFFFFF'
+font = 'sans serif'
+
 " > ~/.streamlit/config.toml
 
--- src/widgets/dock.py
+++ src/widgets/dock.py
@@ -3,4 +3,5 @@ import streamlit as st
 
 def run():
     st.title('Details')
-    st.write("Check out the [code](https://github.com/ryanmcandrew/streamlit-app)")
+    with st.expander('stuff'):
+        st.write("Check out the [code](https://github.com/ryanmcandrew/streamlit-app)")
